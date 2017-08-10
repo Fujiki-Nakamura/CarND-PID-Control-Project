f530910ad37b0b1a7870398082776af4a1a93104
fujiki <abcdef.nakamura@gmail.com>
2017-08-10 14:28:40 +0900

    WIP

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,9 +11,18 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = i_error = d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+    double cte_prev = p_error;
+
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - cte_prev;
 }
 
 double PID::TotalError() {
